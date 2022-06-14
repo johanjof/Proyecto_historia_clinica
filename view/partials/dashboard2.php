5d63e9026540bbc4092e8ea6bb15436555f19319
johanjof <38874102+johanjof@users.noreply.github.com>
2022-06-14 11:17:51 -0500

    dashboard2 nuevo

--- view/partials/dashboard2.php
+++ view/partials/dashboard2.php
@@ -17,24 +17,24 @@
 	<link href='https://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" href="./css/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="./css/style.css"> <!-- Resource style -->
+	<link rel="stylesheet" href="./css/sstyle.css" > <!-- Resource style -->
 	<script src="./js/modernizr.js"></script> <!-- Modernizr -->
   	
 	<title>Dashboard</title>
 </head>
 <body>
-<main id="cd-main-content">
+<!-- <main id="cd-main-content">
 	<section id="cd-intro">
 		<h1>Hola bienvenido a Optilax</h1>
 
 		<header class="cd-header">
 			<div id="cd-logo"><a href="#0"><img src="https://codyhouse.co/demo/full-page-intro-and-navigation/img/cd-logo.svg" alt="Logo"></a></div>
-			<a class="cd-menu-trigger" href="#main-nav">Menu<span></span></a>
+			
 		</header>
 		<div class="cd-blurred-bg"></div>
-	</section> <!-- cd-intro -->
-</main>
-
+	</section>   cd-intro  -->
+<!-- </main>  -->
+<a class="cd-menu-trigger" href="#main-nav">Menu<span></span></a>
 <div class="cd-shadow-layer"></div>
 
 <nav id="main-nav">
@@ -50,7 +50,7 @@
 	  <a href="#" class="collapsible"><span>Estratos</span></a>
 	  <div class="content">
 		  <a href="<?php echo getUrl("Estrato","Estrato","getInsert"); ?>" class="close-menu"><p>Registrar Estratos</p></a>
-		  <a href="<?php echo getUrl("Estrato","Estrato","consult"); ?>" class="close-menu"><p>Consultar Estratos</p></a>
+		  <a href="<?php echo getUrl("Estrato","Estrato","consult"); ?>" ><p>Consultar Estratos</p></a>
 		</div>
 
 		<a href="#" class="collapsible"><span>Generos</span></a>
@@ -62,10 +62,9 @@
         <a href="#" class="collapsible"><span>Usuarios</span></a>
 	  <div class="content">
 		  <a href="#"><p>Registrar Usuarios</p></a>
-		  <a href="#"><p>Consultar Usuarios</p></a>
+		  <a href="#" class="close-menu"><p>Consultar Usuarios</p></a>
 		</div>
-
-        
+ 
 			
 </li>
 	
