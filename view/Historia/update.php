cf54f527e7200181c27d85acc3c34053b1d4a679
hbescobar <95257215+hbescobar@users.noreply.github.com>
2022-06-26 19:00:44 -0500

    historias funcional

--- view/Historia/update.php
+++ view/Historia/update.php
@@ -1,6 +1,6 @@
 <div class="mt-5">
     <center>
-    <h3 class="display-4">Editar Historia Clinica</h3>
+    <h3 class="display-16">Editar Historia Clinica</h3>
 </div>
 
 <div class="mt-5">
@@ -10,36 +10,51 @@
     ?>
     <hr>
 <center>
-<table table style="border: hidden">
-            <tr>
-           <th><font face="Calibri">Documento</font></th> 
-           <th><font face="Calibri">Nombre</font></th>
-           <th><font face="Calibri">Apellido</font></th>
-            </tr>
-
-         <tr>
-            <input type="hidden" name="hist_id" value="<?=$hist['hist_id']?>">
-            <th><?php echo $hist['pac_documento'];?></th>
-            <th><?php echo $hist['pac_nombre'];?></th>
-            <th><?php echo $hist['pac_apellido'];?></th>
-            </tr>   
-        </table>
-
-        <table table style="border: hidden">
-        <tr>
-            <th><font face="Calibri">Telefono:</font></th>
-            <th><font face="Calibri">Genero:</font></th>
-            <th><font face="Calibri">Estrato:</font></th>
-        </tr>
-        <tr>
-        <th><?php echo $hist['pac_telefono'];?>
-            </th>
-            <td>
-                    <div class="row-md-4">
-                   
-                           
-                               
-                            <?php
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+       <b>Documento: </b>
+    </div>
+    <div class="col col-3"> 
+    <b> Nombre</b>
+    </div>
+    <div class="col col-3">
+     <b>Apellido </b>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+      <?php echo $hist['pac_documento'];?> 
+    </div>
+    <div class="col col-3">
+    <?php echo $hist['pac_nombre'];?>
+    </div>
+    <div class="col col-3">
+    <?php echo $hist['pac_apellido'];?>
+    </div>
+  </div>
+  <div class="container">
+  <div class="row">
+    <div class="col col-4">
+       <b>Telefono </b>
+    </div>
+    <div class="col col-3"> 
+    <b> Genero:</b>
+    </div>
+    <div class="col col-3">
+     <b> Estrato </b>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+    <?php echo $hist['pac_telefono'];?>
+    </div>
+    <div class="col col-3"> 
+    <?php
                                 foreach ($generos as $gen){
                                     if ($gen['gen_id']==$hist['gen_id']) {
                                         echo $gen['gen_nombre'];
@@ -50,14 +65,9 @@
                                     
                                 
                             ?>
-                          
-                    </div>
-                </td>
-                <th> <div class="row-md-4">
-                  
-                  
-                     
-                  <?php
+    </div>
+    <div class="col col-3">
+    <?php
                       foreach ($estratos as $str){
                           if ($str['estr_id']==$hist['estr_id']) {
                             echo $str['estr_nombre'];
@@ -68,25 +78,18 @@
                           
                       
                   ?>
-                  </select>
-          </div>
-                  </th>
-              
-  </tr>   
-</table>
+    </div>
+  </div>
+</div>
 
-           
-            
-        </tr>
-        </table>
-        
 
+    
        
 </div>
 <hr>
 <center>
 <div class="mt-3">
-    <form action="<?php echo getUrl("Historia","Historia","postInsert",array("pac_id"=>$pac['pac_id']));?>" method="post">
+    <form action="<?php echo getUrl("Historia","Historia","postUpdate",array("hist_id"=>$hist['hist_id']));?>" method="post">
     <table>
         <tr>
             <th><font face="Calibri">Motivo de visita </font></th>
@@ -94,11 +97,13 @@
             <th><font face="Calibri">Cilindro OD</font></th>
         </tr>
         <tr>
-        <th><input type="text" name="hist_motv" class="form-control" placeholder="Motivo  de visita " required="required"  value="<?php echo $hist['hist_motv'];?>">
+        <input type="hidden" name="hist_id" value="<?php=$hist['hist_id']?>">
+
+        <th><input type="text" name="hist_motv" class="form-control"  value="<?php echo $hist['hist_motv'];?>">
             </th>
-            <th> <input type="number" name="hist_esfod" class="form-control" >
+            <th> <input type="number" name="hist_esfod" class="form-control"  value="<?php echo $hist['hist_esfod'];?>" >
            </th>
-             <th>  <input type="number" name="hist_cilod" class="form-control">
+             <th>  <input type="number" name="hist_cilod" class="form-control"  value="<?php echo $hist['hist_cilod'];?>">
             </th>
         </tr>
         </table>
@@ -109,11 +114,11 @@
             <th><font face="Calibri">Cilindro OI</font></th>
         </tr>
         <tr>
-        <th><input type="number" name="hist_ejeod" class="form-control">
+        <th><input type="number" name="hist_ejeod" class="form-control" value="<?php echo $hist['hist_ejeod'];?>">
             </th>
-            <th> <input type="number" name="hist_esfoi"class="form-control" >
+            <th> <input type="number" name="hist_esfoi"class="form-control" value="<?php echo $hist['hist_esfoi'];?>" >
            </th>
-             <th>  <input type="number" name="hist_ciloi" class="form-control">
+             <th>  <input type="number" name="hist_ciloi" class="form-control" value="<?php echo $hist['hist_ciloi'];?>">
             </th>
         </tr>
         </table>
@@ -124,11 +129,11 @@
             <th><font face="Calibri">Diagnostico  ojo izquierdo</font></th>
         </tr>
         <tr>
-        <th><input type="number" name="hist_ejeoi" class="form-control">
+        <th><input type="number" name="hist_ejeoi" class="form-control" value="<?php echo $hist['hist_ejeoi'];?>">
             </th>
-            <th> <input type="text" name="hist_diaod"class="form-control" >
+            <th> <input type="text" name="hist_diaod"class="form-control" value="<?php echo $hist['hist_diaod'];?>" >
            </th>
-             <th>  <input type="text" name="hist_diaoi" class="form-control">
+             <th>  <input type="text" name="hist_diaoi" class="form-control" value="<?php echo $hist['hist_diaoi'];?>">
             </th>
         </tr>
         </table>
@@ -138,8 +143,10 @@
         <th> 
 
         <font face="Calibri">Recomendaciones:</font>
+        <input type="text" name="hist_recom" class="form-control" value="<?php echo $hist['hist_recom'];?>">
+
         <br>
-        <textarea  name="hist_recom"> </textarea> 
+       
 
         
     </th>
--- view/Historia/verHistoria.php
+++ view/Historia/verHistoria.php
@@ -1,40 +1,28 @@
 <div class="mt-5">
+    <hr>
     
-    <h3 class="display-16" style="margin-left:16% " >Informacion  Historia medica </h3>
+    <h3 class="display-16" style="margin-left:16%"  class="wrapper d-flex">Información  Historia medica </h3>
 </div>
 
-
+<center>
 <div class="mt-5">
     <?php
        
         foreach($historias as $hist){
     ?>
     
-<center>
 
-<div class="container">
+
+<div class="container" >
   <div class="row">
     <div class="col col-4">
-       <b>No. Historia Clinica</b>
+       <b>No. Clinica </b>  <?php echo $hist['hist_id']?>
     </div>
     <div class="col col-3"> 
-    <b> Nombre</b>
+    <b> Paciente</b>  <?php echo $hist['pac_nombre'];?>  <?php echo $hist['pac_apellido'];?>
     </div>
     <div class="col col-3">
-     <b>Genero </b>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <div class="row">
-    <div class="col col-4">
-    <?=$hist['hist_id']?>
-    </div>
-    <div class="col col-3">
-    <?php echo $hist['pac_nombre'];?>
-    </div>
-    <div class="col col-3">
-    <?php
+     <b>Genero </b>  <?php
                                 foreach ($generos as $gen){
                                     if ($gen['gen_id']==$hist['gen_id']) {
                                         echo $gen['gen_nombre'];
@@ -50,76 +38,131 @@
 </div>
 
 
+
        
 
 <hr>
 
-<table>
-
-
-        <tr>
-            <th><font face="Calibri">Motivo de visita </font></th>
-            <th><font face="Calibri">Esfera OD</font></th>
-            <th><font face="Calibri">Cilindro OD</font></th>
-        </tr>
-        <tr>
-        <input type="hidden" name="hist_id" value="<?=$hist['hist_id']?>">
-        
-        <th><input type="text" name="hist_motv" class="form-control" readonly value="<?php echo $hist['hist_motv'];?>"></th>
-            </th>
-            <th> <input type="number" name="hist_esfod" class="form-control" readonly value="<?php echo $hist['hist_esfod'];?>"></th>
-           </th>
-             <th>  <input type="number" name="hist_cilod" class="form-control" readonly value="<?php echo $hist['hist_cilod'];?>"></th>
-            </th>
-        </tr>
-        </table>
-        <table>
-        <tr>
-            <th><font face="Calibri">Eje OD</font></th>
-            <th><font face="Calibri">Esfera OI</font></th>
-            <th><font face="Calibri">Cilindro OI</font></th>
-        </tr>
-        <tr>
-        <th><input type="number" name="hist_ejeod" class="form-control" readonly value="<?php echo $hist['hist_ejeod'];?>"></th>
-            </th>
-            <th> <input type="number" name="hist_esfoi"class="form-control"  readonly value="<?php echo $hist['hist_esfoi'];?>"></th>
-           </th>
-             <th>  <input type="number" name="hist_ciloi" class="form-control" readonly value="<?php echo $hist['hist_ciloi'];?>"></th>
-            </th>
-        </tr>
-        </table>
-        <table>
-        <tr>
-            <th><font face="Calibri">Eje OI</font></th>
-            <th><font face="Calibri">Diagnostico ojo derecho</font></th>
-            <th><font face="Calibri">Diagnostico ojo izquierdo</font></th>
-        </tr>
-        <tr>
-        <th><input type="number" name="hist_ejeoi" class="form-control"  readonly value="<?php echo $hist['hist_ejeoi'];?>"></th>
-            </th>
-            <th> <input type="text" name="hist_diaod"class="form-control"   readonly value="<?php echo $hist['hist_diaod'];?>"></th>
-           </th>
-             <th>  <input type="text" name="hist_diaoi" class="form-control"  readonly value="<?php echo $hist['hist_diaoi'];?>"></th>
-            </th>
-        </tr>
-        </table>
-
-            <div class="col-md-4">
-                <label class="form-label">Recomendaciones</label>
-                <input type="text" name="hist_recom" class="form-control"   readonly value="<?php echo $hist['hist_recom'];?>"></th>
-            </div>
-           
-        </div>
-        
-        </table>
-
-
-        <div class="col-md-4 mt-4">
-        <a href="<?php echo getUrl("Historia","Historia","Getupdate",array("pac_id"=>$hist['pac_id']))?>"><button class="btn btn-info">Editar</button></a>
-<a href="<?php echo getUrl("Paciente","Paciente","consult"); ?>"><button class="btn btn-info">Volver</button></a>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+       <b> Motivo de visita  </b>
+    </div>
+    <div class="col col-3"> 
+    <b> Esfera OD</b>
+    </div>
+    <div class="col col-3">
+     <b>Cilindro OD </b>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+    <?php echo $hist['hist_motv'];?>
+    </div>
+    <div class="col col-3"> 
+    <?php echo $hist['hist_esfod'];?>
+    </div>
+    <div class="col col-3">
+    <?php echo $hist['hist_cilod'];?>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+       <b> Eje OD</b>
+    </div>
+    <div class="col col-3"> 
+    <b> Esfera OI</b>
+    </div>
+    <div class="col col-3">
+     <b>Cilindro OI </b>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+    <?php echo $hist['hist_ejeod'];?>
+    </div>
+    <div class="col col-3"> 
+    <?php echo $hist['hist_esfoi'];?>
+    </div>
+    <div class="col col-3">
+    <?php echo $hist['hist_ciloi'];?>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+    <b> Eje OI </b>
+    </div>
+    <div class="col col-3"> 
+    <b> Diagnostico ojo derecho </b>
+    </div>
+    <div class="col col-3">
+    <b> Diagnostico ojo izquierdo </b>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+    <?php echo $hist['hist_ejeoi'];?>
+    </div>
+    <div class="col col-3"> 
+    <?php echo $hist['hist_diaod'];?>
+    </div>
+    <div class="col col-3">
+    <?php echo $hist['hist_diaoi'];?>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+     <b> Recomendaciones </b>
+    </div>
+    <div class="col col-3"> 
+   
+    </div>
+    <div class="col col-3">
+      
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col col-4">
+    <?php echo $hist['hist_recom'];?>
+    </div>
+    <div class="col col-3"> 
+   
+    </div>
+    <div class="col col-3">
+      
+    </div>
+  </div>
 </div>
 
+
+       
+       
+        
+  
+      
+
+
+
     </div>
+
+    <div class="col-md-4 mt-4">
+        <a href="<?php echo getUrl("Historia","Historia","Getupdate",array("hist_id"=>$hist['hist_id']))?>"><button class="btn btn-info">Editar</button></a>
+<a href="<?php echo getUrl("Paciente","Paciente","consult"); ?>"><button class="btn btn-info">Volver</button></a> <br><br><br><br><br><br><br><br>
+</div>
 
     <?php
         }
